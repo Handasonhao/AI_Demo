273e8803fb5eb9e3e78864363dd9720b1d62cd8d
Handasonhao <1546356515@qq.com>
2021-04-28 11:17:36 +0800

    提交测试

---
+++ my_first_demo/222.py
@@ -0,0 +1,8 @@
+'''
+Author       : Wang.HH
+Date         : 2021-04-28 11:17:15
+LastEditTime : 2021-04-28 11:17:16
+LastEditors  : Wang.HH
+Description  : your description
+FilePath     : /AI_Demo/my_first_demo/222.py
+'''
