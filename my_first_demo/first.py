466abeb3034f901998d8f55b60d44e1a408e10c3
Handasonhao <1546356515@qq.com>
2021-04-28 10:43:42 +0800

    🎉 [初次提交]

---
+++ my_first_demo/first.py
@@ -0,0 +1,8 @@
+'''
+Author       : Wang.HH
+Date         : 2021-04-28 10:39:59
+LastEditTime : 2021-04-28 10:40:00
+LastEditors  : Wang.HH
+Description  : your description
+FilePath     : /AI_Demo/my_first_demo/first.py
+'''
