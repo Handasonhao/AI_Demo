14062f7197315d44ceb238caacbccd718f6a3eef
Handasonhao <1546356515@qq.com>
2021-04-29 16:58:59 +0800

    测试

--- my_first_demo/first.py
+++ my_first_demo/first.py
@@ -1,9 +1,9 @@
 '''
 Author       : Wang.HH
 Date         : 2021-04-28 10:39:59
-LastEditTime : 2021-04-29 16:56:38
+LastEditTime : 2021-04-29 16:58:30
 LastEditors  : Wang.HH
 Description  : your description
 FilePath     : /AI_Demo/my_first_demo/first.py
 '''
-print('10012')
+print('1001fasdf2')
