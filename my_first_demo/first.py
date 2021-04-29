00272956efe25101667945e875b74f19da6abfb1
Handasonhao <1546356515@qq.com>
2021-04-29 14:17:11 +0800

    66666

--- my_first_demo/first.py
+++ my_first_demo/first.py
@@ -1,9 +1,9 @@
 '''
 Author       : Wang.HH
 Date         : 2021-04-28 10:39:59
-LastEditTime : 2021-04-29 14:05:46
+LastEditTime : 2021-04-29 14:17:02
 LastEditors  : Wang.HH
 Description  : your description
 FilePath     : /AI_Demo/my_first_demo/first.py
 '''
-print('1111')
+print('2222')
