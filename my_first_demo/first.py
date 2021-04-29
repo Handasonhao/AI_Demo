3f36b9fcb5cd20660249e6e982e95c183a5564bb
Handasonhao <1546356515@qq.com>
2021-04-29 15:54:49 +0800

    测试

--- my_first_demo/first.py
+++ my_first_demo/first.py
@@ -1,9 +1,9 @@
 '''
 Author       : Wang.HH
 Date         : 2021-04-28 10:39:59
-LastEditTime : 2021-04-29 15:45:29
+LastEditTime : 2021-04-29 15:54:17
 LastEditors  : Wang.HH
 Description  : your description
 FilePath     : /AI_Demo/my_first_demo/first.py
 '''
-print('2211111112')
+print('1112')
