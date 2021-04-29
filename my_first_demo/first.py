3dee1e4edd39066222432133a99b5e70cf2ec578
Handasonhao <1546356515@qq.com>
2021-04-29 19:34:18 +0800

    打算大苏打

--- my_first_demo/first.py
+++ my_first_demo/first.py
@@ -6,8 +6,4 @@ LastEditors  : Wang.HH
 Description  : your description
 FilePath     : /AI_Demo/my_first_demo/first.py
 '''
-<<<<<<< HEAD
-print('hhhh')
-=======
-print('1000000000012')
->>>>>>> fe9c5c4f552233cbaa8ce6fef4f81079cea0f4f3
+print('hhh1111h')
