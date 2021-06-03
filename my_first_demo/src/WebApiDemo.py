3526b7b176d89fc8b56d449df59205e067b9537b
Handasonhao <1546356515@qq.com>
2021-06-03 14:49:20 +0800

    🤔 完成了api字符串 todo json对象传输

--- my_first_demo/src/WebApiDemo.py
+++ my_first_demo/src/WebApiDemo.py
@@ -1,13 +1,40 @@
 '''
 Author       : Wang.HH
 Date         : 2021-06-02 08:19:30
-LastEditTime : 2021-06-02 12:53:52
+LastEditTime : 2021-06-03 14:48:33
 LastEditors  : Wang.HH
 Description  : your description
 FilePath     : /AI_Demo/my_first_demo/src/WebApiDemo.py
 '''
-def simple_app(envirron, start_response):
-  status = '200 OK'
-  response_headers = [('Content-type', 'text/plain')]
-  start_response(status,response_headers)
-  return ["hello shit!\n"]
+
+class my_app:
+  def __init__(self,environ,start_response):
+    self.environ = environ
+    self.start = start_response
+  
+  def __iter__(self):
+    path = self.environ["PATH_INFO"]
+    if path == '/':
+      return iter(self.Get_Index())
+    elif path == '/hello':
+      return iter(self.Get_Hello())
+    else:
+      return iter(self.NotFound())
+  
+  def Get_Index(self):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    self.start(status,response_headers)
+    return ["hello shit!\n".encode('utf-8')]
+  
+  def Get_Hello(self):
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/plain')]
+    self.start(status,response_headers)
+    return ["hello world!\n".encode('utf-8')]
+
+  def NotFound(self):
+    status = '404 Not Found'
+    response_headers = [('Content-type', 'text/plain')]
+    self.start(status,response_headers)
+    return ["Not Found!\n".encode('utf-8')]
--- my_first_demo/src/webserve.py
+++ my_first_demo/src/webserve.py
@@ -1,19 +1,20 @@
 '''
 Author       : Wang.HH
 Date         : 2021-06-02 11:08:37
-LastEditTime : 2021-06-02 12:55:03
+LastEditTime : 2021-06-03 14:15:55
 LastEditors  : Wang.HH
 Description  : your description
 FilePath     : /AI_Demo/my_first_demo/src/webserve.py
 '''
 import wsgiref.simple_server as wss
-from WebApiDemo import simple_app as app
+from WebApiDemo import my_app as app
 
-mk = wss.make_server
-demo_app = wss.demo_app
-httpd = mk('127.0.0.1',8086,demo_app)
-sa = httpd.socket.getsockname()
-print(httpd.socket)
-print(sa)
-print('http://{0}:{1}/'.format(*sa))
-httpd.serve_forever()
+if __name__ == '__main__':
+  mk = wss.make_server
+  demo_app = wss.demo_app
+  httpd = mk('127.0.0.1',8086,app)
+  sa = httpd.socket.getsockname()
+  print(httpd.socket)
+  print(sa)
+  print('http://{0}:{1}/'.format(*sa))
+  httpd.serve_forever()
